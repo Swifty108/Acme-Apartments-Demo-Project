f434862f025b3938e86662556282551199d8ca05
swifty108 <rajnarayanan2020@gmail.com>
2020-10-21 13:47:27 -0400

    fix a few bugs

---
+++ Peach Grove Apartments Demo Project/Controllers/AppUserAccountController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Peach_Grove_Apartments_Demo_Project.Data;
+using Peach_Grove_Apartments_Demo_Project.Models;
+
+namespace Peach_Grove_Apartments_Demo_Project.Controllers
+{
+    public class AppUserAccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppUserAccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AppAppUserAccount
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Applications.Include(a => a.AptUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Maintenance()
+        {
+            var applicationDbContext = _context.Applications.Include(a => a.AptUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: AppAppUserAccount/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .Include(a => a.AptUser)
+                .FirstOrDefaultAsync(m => m.ApplicationId == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // GET: AppAppUserAccount/Create
+        public IActionResult Create()
+        {
+            ViewData["AptUserId"] = new SelectList(_context.AptUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: AppAppUserAccount/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ApplicationId,AptUserId,Occupation,Income,ReasonForMoving,SSN,Room,Price")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                application.ApplicationId = Guid.NewGuid();
+                _context.Add(application);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AptUserId"] = new SelectList(_context.AptUsers, "Id", "Id", application.AptUserId);
+            return View(application);
+        }
+
+        // GET: AppAppUserAccount/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications.FindAsync(id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+            ViewData["AptUserId"] = new SelectList(_context.AptUsers, "Id", "Id", application.AptUserId);
+            return View(application);
+        }
+
+        // POST: AppAppUserAccount/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ApplicationId,AptUserId,Occupation,Income,ReasonForMoving,SSN,Room,Price")] Application application)
+        {
+            if (id != application.ApplicationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(application);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationExists(application.ApplicationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AptUserId"] = new SelectList(_context.AptUsers, "Id", "Id", application.AptUserId);
+            return View(application);
+        }
+
+        // GET: AppAppUserAccount/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .Include(a => a.AptUser)
+                .FirstOrDefaultAsync(m => m.ApplicationId == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // POST: AppAppUserAccount/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var application = await _context.Applications.FindAsync(id);
+            _context.Applications.Remove(application);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApplicationExists(Guid id)
+        {
+            return _context.Applications.Any(e => e.ApplicationId == id);
+        }
+    }
+}
