5c225d0b3186a7b4733382369e7f30710ca5aed3
rajndev <rajnsoftwaredev@gmail.com>
2022-01-19 15:18:55 -0500

    Clean up the project

--- src/AcmeApartments.Web/HelperClasses/AutoMappingProfile.cs
+++ src/AcmeApartments.Web/HelperClasses/AutoMappingProfile.cs
@@ -13,72 +13,53 @@ namespace AcmeApartments.Web.HelperClasses
         {
             //Home controller
 
-            //CreateMap<FloorPlansViewModel, FloorPlansViewModelDTO>();
             CreateMap<FloorPlansViewModelDTO, FloorPlansViewModel>().ReverseMap();
 
-            //CreateMap<ApplyViewModelDTO, ApplyBindingModel>();
             CreateMap<ApplyBindingModel, ApplyViewModelDTO>().ReverseMap();
 
-            //CreateMap<ApplyViewModel, ApplyBindingModel>();
             CreateMap<ApplyBindingModel, ApplyViewModel>().ReverseMap();
 
             CreateMap<AppUserContactBindingModel, AppUserContactViewModel>().ReverseMap();
-            //CreateMap<AppUserContactViewModel, AppUserContactBindingModel>();
 
             //Home Controller Logic Class
 
             CreateMap<FloorPlan, FloorPlanDTO>().ReverseMap();
-            //CreateMap<FloorPlanDTO, FloorPlan>();
 
             //Manager controller
 
-            //CreateMap<ApplicationViewModel, Application>();
             CreateMap<Application, ApplicationViewModel>().ReverseMap();
 
             CreateMap<ApplicationDTO, ApplicationBindingModel>().ReverseMap();
-            //CreateMap<ApplicationBindingModel, ApplicationDTO>();
 
-            //CreateMap<ApplicationViewModel, ApplicationBindingModel>();
             CreateMap<ApplicationBindingModel, ApplicationViewModel>().ReverseMap();
 
-            //CreateMap<MaintenanceRequestViewModel, MaintenanceRequest>();
             CreateMap<MaintenanceRequest, MaintenanceRequestViewModel>().ReverseMap();
 
             CreateMap<MaintenanceRequestEditDTO, MaintenanceRequestEditBindingModel>().ReverseMap();
-            //CreateMap<MaintenanceRequestEditBindingModel, MaintenanceRequestEditDTO>();
 
-            //CreateMap<MaintenanceRequestEditViewModel, MaintenanceRequestEditBindingModel>();
             CreateMap<MaintenanceRequestEditBindingModel, MaintenanceRequestEditViewModel>().ReverseMap();
 
             //Manager Controller Logic
 
-            //CreateMap<ApplicationDTO, Application>();
             CreateMap<Application, ApplicationDTO>().ReverseMap();
 
             //Applicant controller
 
             CreateMap<ApplicantContactBindingModel, ApplicantContactViewModel>().ReverseMap();
-            //CreateMap<ApplicantContactViewModel, ApplicantContactBindingModel>();
 
             //Resident controller
 
             CreateMap<ReviewBindingModel, ReviewViewModelDTO>().ReverseMap();
-            //CreateMap<ReviewViewModelDTO, ReviewBindingModel>();
 
             CreateMap<ReviewBindingModel, ReviewViewModel>().ReverseMap();
-            //CreateMap<ReviewViewModel, ReviewBindingModel>();
 
             CreateMap<ResidentContactBindingModel, ResidentContactViewModel>().ReverseMap();
-            //CreateMap<ResidentContactViewModel, ResidentContactBindingModel>();
 
             CreateMap<NewMaintenanceRequestDTO, NewMaintenanceRequestBindingModel>().ReverseMap();
-            //CreateMap<NewMaintenanceRequestBindingModel, NewMaintenanceRequestDTO>();
 
             CreateMap<NewMaintenanceRequestBindingModel, NewMaintenanceRequestViewModel>().ReverseMap();
-            //CreateMap<NewMaintenanceRequestViewModel, NewMaintenanceRequestBindingModel>();
 
             CreateMap<PaymentsViewModelDTO, PaymentsViewModel>().ReverseMap();
-            //CreateMap<PaymentsViewModel, PaymentsViewModelDTO>();
         }
     }
 }
